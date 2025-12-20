4d84909a8178f7aed3d446876a17cdda23b3bd39
AKR-code <25mcce10@uohyd.ac.in>
2025-12-20 19:42:09 +0530

    implementing base program with reader

---
+++ Main.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define EXPECTARG 2
+#define EXITCHAR 0
+
+typedef struct {
+	int value;
+	char name[50];
+} VAR; 
+
+void aboutTool ();
+void loop (FILE *, int);
+char reader (FILE *);
+char lexer (char *, int);
+
+int main (int argc, char *argv[]) {
+	if (argc > EXPECTARG) {
+		printf("Error: Too many targets\n");
+		for (int i = 1; i < argc; i++) {
+			printf("       %s\n", argv[i]);
+		}
+		return 1;
+	}
+	FILE *inputLoc = NULL;
+	if (argc == 1) {
+		inputLoc = stdin;
+		aboutTool();
+	} else {
+		inputLoc = fopen(argv[1], "r");
+		if (inputLoc == NULL) {
+			printf("Error: Target not found in present working directory\n");
+			return 1;
+		}
+	}//end of deciding
+	loop(inputLoc, !(argc - 1));
+}
+
+void aboutTool () {
+	printf("SIL V0.1\n");
+	printf("this is a prototype for LAMDA\n");
+	return;
+}
+
+void loop(FILE *inputLoc, int isShell) {
+	char status = EXITCHAR;
+	VAR *varTable = NULL;
+	do {
+		if (isShell) printf("SIL: ");
+		status = reader(inputLoc);
+	} while (status != EXITCHAR);
+}
+
+char reader(FILE *inputLoc) {
+	int size = 32;
+    char *string = malloc(size * sizeof(char));
+	int c;
+	int index = 0;
+	int comment = 0;
+	int firstChar = 0;
+	while ((c = fgetc(inputLoc)) != '\n' && c != EOF) {
+		firstChar = (firstChar || (c != ' ' && c != '\t' ) ? 1 : 0);
+		if (!firstChar) continue;
+		comment = (c == '@' ? !comment : comment);
+		if (comment || c == '@') continue;
+		string[index] = (char)c;
+		index++;
+		if (index + 1 >= size) {
+			size *= 2;
+			char *temp = realloc(string, size * sizeof(char));
+			if (!temp) {
+				free(string);
+				return EXITCHAR;
+			}
+			string = temp;
+		}
+	} 
+	if (c == EOF && index == 0) {
+		free(string);
+		return EXITCHAR;
+	}
+	string[index] = '\0';
+	char status = lexer(string, index);
+	free(string);
+	string = NULL;
+	return status;
+}
+
+char lexer (char *string, int len) {
+	printf("-%s-\n", string);
+	return 1;
+}
